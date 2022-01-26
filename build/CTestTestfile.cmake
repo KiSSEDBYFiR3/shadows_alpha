5612728dc22f0508e7b4b1748bcdb28afbfc8d15
Stanislav Moiseev <72256017+HopelessFlavor@users.noreply.github.com>
2022-01-26 23:00:30 +0300

    final update

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Workspace/project_alpha_01/shadows_0.3a/shadows_alpha
+# Build directory: C:/Workspace/project_alpha_01/shadows_0.3a/shadows_alpha/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
